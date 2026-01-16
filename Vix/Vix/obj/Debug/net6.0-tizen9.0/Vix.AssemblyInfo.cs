57fa70b92ff27feaa98c6d3d5fdc138b0a900aa1
nicogatti <ngatti@ext.laligatech.com>
2026-01-16 12:00:06 -0300

    added gitignore

--- Vix/Vix/obj/Debug/net6.0-tizen9.0/Vix.AssemblyInfo.cs
+++ Vix/Vix/obj/Debug/net6.0-tizen9.0/Vix.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vix")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7ab569ce59f0e7ee25e721c4f150a498e02252d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466ff2e9d60effc6c3ab0fa9a330c13f0e27cc0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vix")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vix")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
